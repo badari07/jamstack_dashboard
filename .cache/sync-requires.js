2f6d46286fb96076209d2314794898a1716d4131
badari07 <badari0793@gmail.com>
2020-05-28 18:45:42 +0530

    feat: oncreate page hoot for gatsby

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Volumes/Baddy-T5/front-end/FE-2018/FE_master/jamstack_dashboard/.cache/dev-404-page.js"))),
+  "component---src-pages-dashboard-js": hot(preferDefault(require("/Volumes/Baddy-T5/front-end/FE-2018/FE_master/jamstack_dashboard/src/pages/dashboard.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Volumes/Baddy-T5/front-end/FE-2018/FE_master/jamstack_dashboard/src/pages/index.js")))
 }
 
---
+++ src/pages/dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const DashBoard = () => (
+  <Layout>
+    <p>Todo: create a dashboard</p>
+  </Layout>
+);
+
+export default DashBoard;
