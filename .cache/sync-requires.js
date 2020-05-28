982a1415b7a31faf2b1859cd9d998409d82a2952
badari07 <badari0793@gmail.com>
2020-05-29 00:07:18 +0530

    fix

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,7 +7,6 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Volumes/Baddy-T5/front-end/FE-2018/FE_master/jamstack_dashboard/.cache/dev-404-page.js"))),
   "component---src-pages-dashboard-js": hot(preferDefault(require("/Volumes/Baddy-T5/front-end/FE-2018/FE_master/jamstack_dashboard/src/pages/dashboard.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Volumes/Baddy-T5/front-end/FE-2018/FE_master/jamstack_dashboard/src/pages/index.js"))),
-  "component---src-pages-todo-js": hot(preferDefault(require("/Volumes/Baddy-T5/front-end/FE-2018/FE_master/jamstack_dashboard/src/pages/todo.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/Volumes/Baddy-T5/front-end/FE-2018/FE_master/jamstack_dashboard/src/pages/index.js")))
 }
 
