7052bc507efe9874d5aa7deaa83ebcf34fe1fd03
badari07 <badari0793@gmail.com>
2020-05-29 09:49:03 +0530

    feat:final

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Blog = () => {
-  return <h1>this is blog</h1>;
+  return (
+    <>
+      <h1>Blog</h1>
+      <h2>Badari Narayan</h2>
+      <p>
+        I'm a software developer, currently working in JavaSystems.check my
+        <span>
+          <a href="https://badari.netlify.app/"> Portfolio. </a>
+        </span>
+        This application uses authenticate users with netlify identity. Todo app
+        which involved with serverless function. Gatsby for the Static site
+        generators.Graphql for api.
+      </p>
+    </>
+  );
 };
 
 export default Blog;
