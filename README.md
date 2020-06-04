54d6c277292a04c6463d01e423f2d839b0f1eec6
badari07 <badari0793@gmail.com>
2020-06-05 03:00:24 +0530

    feat: adding readme & instruction

---
+++ README.md
@@ -0,0 +1,37 @@
+###JAMstack App
+
+Jamstack app with Authenticate users with Netlify Identity.
+
+## Instructions to Run App.
+
+After Cloning or downloading zip file, Run
+
+```bash
+yarn or npm install
+```
+
+for downloading the dependencies of project.
+
+## Netlify
+
+Install netlify-cli by
+
+```bash
+yarn add netlify-cli / npm i netlify-cli
+```
+
+After installing run
+
+```bash
+ netlify login
+```
+
+You should to register to netlify.
+
+## To run the App in development mode run
+
+```bash
+netlify dev
+```
+
+You can view the App in 'http://localhost:8888'
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/Layout";
 export default () => (
   <Layout>
     <>
-      <h1>Instructions to use App</h1>
+      <h1>Instructions to use App.</h1>
       <Link to="/dashboard" className="button-dashboard">
         <span>DashBoard </span>
       </Link>
@@ -32,6 +32,40 @@ export default () => (
           <p>
             OtherWise you have to <b>Register </b> First by <b>Sign up </b>and
             confirm your e-mail address. Then you can Log In to app.
+          </p>
+        </li>
+      </ol>
+      <h1>Instructions to Run App.</h1>
+      <ol>
+        <li>
+          <p>
+            After Cloning or downloading zip file, Run{" "}
+            <b>yarn or npm install</b> for downloading the dependencies of
+            project.
+          </p>
+        </li>
+        <li>
+          <p>
+            <b>yarn add netlify-cli / npm i netlify-cli </b>. After installing
+            run <b>netlify login</b>. You should to register to netlify.
+          </p>
+        </li>
+        <li>
+          <p>
+            Then, run <b>netlify dev</b>. To run the App in development mode.
+          </p>
+        </li>
+        <li>
+          <p>
+            You can view the App in{" "}
+            <u>
+              <b>'http://localhost:8888'</b>
+            </u>
+          </p>
+        </li>
+        <li>
+          <p>
+            You can read it in <b>README.md</b> file inside the project app.
           </p>
         </li>
       </ol>
